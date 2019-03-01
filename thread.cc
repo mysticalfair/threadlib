636553dbffd5ca83fd2289165e498b3159fc5248
acao56 <30483555+acao56@users.noreply.github.com>
2019-02-28 19:22:43 -0500

    libinit, create, yield, and execute function, removed handler

--- thread.cc
+++ thread.cc
@@ -23,8 +23,18 @@ typedef struct cv_t {
 
 static bool initialized = false;
 static queue<thread_t*> ready;
-static ucontext_t* handler;
-static thread_t* currentThread;
+static thread_t* current_thread;
+static int num_threads = 0;
+
+static int execute_function(thread_startfunc_t func, void *arg) {	
+	func(arg);
+	if(!ready.empty()) {
+		thread_t* next_thread = ready.front();
+		ready.pop();
+		swapcontext(current_thread->ucon, handler);
+	}
+	return 0;
+}
 
 int thread_libinit(thread_startfunc_t func, void *arg) {
 	if(initialized) {
@@ -33,33 +43,38 @@ int thread_libinit(thread_startfunc_t func, void *arg) {
 	initialized = true;
 
 	thread_create(func, arg);
-  handler = new uncontext_t;
-  getcontext(handler);
-  swapcontext(handler, current->ucon);
-  while (!ready.empty())
-  {
-    if (running->finished)
-    {
-      //delete thread
-    }
-    current = ready.pop();
-    swapcontext(handler, current-> ucon);
-  }
-  cout << "Thread library exiting. \n";
-  exit(0);
+	thread_t* initial_thread = ready.front();
+	ready.pop();
+
+	setcontext(initial_thread->ucon);
+
+	current_thread = initial_thread;
+	execute_function(func, arg);
+
+	return 0;
 }
 
 int thread_create(thread_startfunc_t func, void *arg) {
 	thread_t *new_thread = new thread_t;
 	new_thread->stack = new char [STACK_SIZE];
+	new_thread->ucon = new ucontext_t;
+	getcontext(new_thread->ucon);
 	new_thread->ucon->uc_stack = new_thread->stack;
 	new_thread->ucon->uc_stack = NULL;
-	// makecontext(t->ucontext_ptr)
-
+	makecontext(new_thread->ucon, (void (*)()) execute_function, 2, func, arg);
+	num_threads++;
+	new_thread->id = num_threads;
 	return 0;
 }
 
 int thread_yield(void) {
+	ready.push(current_thread);
+	if(!ready.empty()) {
+		thread_t* next_thread = ready.front();
+		ready.pop();
+		swapcontext(current_thread->ucon, handler);
+		current_thread = next_thread;
+	}
 	return 0;
 }
 
