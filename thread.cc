6076691698f7c29191319a38239bece5f2be1f62
Joanna Li <jll77@duke.edu>
2019-02-28 22:07:13 -0500

    finished

--- thread.cc
+++ thread.cc
@@ -10,7 +10,6 @@ typedef struct thread_t {
 } thread_t;
 
 typedef struct lock_t {
-    int flag;
     thread_t* owner;
     queue<thread_t*> q;
 } lock_t;
@@ -27,7 +26,7 @@ static int num_threads = 0;
 static map<unsigned int, lock_t*> lock_map;
 static map<unsigned int, cv_t*> cv_map;
 
-static int execute_function(thread_startfunc_t func, void *arg) {	
+static int execute_function(thread_startfunc_t func, void *arg) {
 	func(arg);
 	if(!ready.empty()) {
 		thread_t* next_thread = ready.front();
@@ -72,7 +71,7 @@ int thread_yield(void) {
 	ready.push(current_thread);
 	thread_t* next_thread = ready.front();
 	ready.pop();
-	swapcontext(current_thread->ucon, handler);
+	swapcontext(current_thread->ucon, next_thread->ucon);
 	current_thread = next_thread;
 	return 0;
 }
@@ -83,7 +82,7 @@ int thread_lock(unsigned int lock) {
 	if(temp_it == lock_map.end()) {
 		lock_t* new_lock = new lock_t;
 		new_lock->owner = current_thread;
-		new_lock->flag = 1;
+		//new_lock->flag = 1;
 		new_lock->q = new queue<thread_t*>
 		lock_map.insert(temp_it, std::pair<unsigned int, lock_t>(lock, new_lock));
 	}
@@ -96,22 +95,88 @@ int thread_lock(unsigned int lock) {
 			lock_map.at(lock)->owner = current_thread;
 		}
 	}
-	intrrupt_enable();
+	interrupt_enable();
 	return 0;
 }
 
 int thread_unlock(unsigned int lock) {
+  interrupt_disable();
+  map<unsigned int, lock_t*>::const_iterator temp_it = lock_map.find(lock);
+  if(temp_it == lock_map.end()) {
+    return -1;
+  }
+  else{
+    lock_t foundLock = temp_it->second;
+    if (foundLock.owner = NULL)
+      return -1;
+    if (foundLock.owner->id == current.id)
+        foundLock.owner = NULL;
+    else{
+      return -1;
+    }
+
+  }
+  }
+  interrupt_enable();
 	return 0;
 }
 
 int thread_wait(unsigned int lock, unsigned int cond) {
-	return 0;
+  //unlocks, sleeps, wake up with lock
+  if (thread_unlock(lock))
+  {
+    map<unsigned int, cv_t*>::const_iterator temp_it = cv_map.find(cond);
+    if(temp_it == cv_map.end()) {
+  		cv_t* new_cv = new cv_t;
+      new_cv->q = new queue<thread_t*>;
+      new_cv->lock = lock;
+      new_cv->q.push(current_thread);
+      cv_map.insert(temp_it, std::pair<unsigned int, cv_t>(cond, new_cv));
+
+    }
+    else{
+      cv_t* foundCV = temp_it->second;
+      foundCV->q.push(current_thread);
+    }
+    thread_t* temp = ready.front(); // if front is empty will throw error
+    ready.pop()
+    swapcontext(current->ucon, temp->ucon)
+    current_thread = temp_thread;
+    return thread_lock(lock);
+  }
+	return -1;
 }
 
 int thread_signal(unsigned int lock, unsigned int cond) {
-	return 0;
+  map<unsigned int, cv_t*>::const_iterator temp_it = cv_map.find(cond);
+  if(temp_it != cv_map.end())
+  {
+    cv_t* foundCV = temp_it->second;
+    queue<thread_t*> temp = foundCV->q;
+    while(!temp.empty())
+    {
+      ready.push(temp.front());
+      temp.pop();
+      break;
+    }
+    return 0;
+  }
+	return -1;
 }
 
 int thread_broadcast(unsigned int lock, unsigned int cond) {
-	return 0;
+
+  map<unsigned int, cv_t*>::const_iterator temp_it = cv_map.find(cond);
+  if(temp_it != cv_map.end())
+  {
+    cv_t* foundCV = temp_it->second;
+    queue<thread_t*> temp = foundCV->q;
+    while(!temp.empty())
+    {
+      ready.push(temp.front());
+      temp.pop();
+    }
+    return 0;
+  }
+  return -1;
 }
